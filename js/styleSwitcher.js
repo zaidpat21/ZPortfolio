c1c34bc58ee1d83454a8c62bb65248542eec794f
zaidpat21 <zaidpat21@gmail.com>
2021-01-19 21:46:48 +0530

    First commit for my personal portfolio

---
+++ js/styleSwitcher.js
@@ -0,0 +1,41 @@
+const links = document.querySelectorAll(".alternate-style"),
+      totalLinks = links.length;
+
+function setActiveStyle(color)
+{
+    for(let i = 0; i < totalLinks; i++)
+    {
+        if(color === links[i].getAttribute("title"))
+        {
+            console.log(color);
+            links[i].removeAttribute("disabled");
+        }
+        else
+        {
+            links[i].setAttribute("disabled", "true");
+        }
+    }
+}
+
+// Body Skin Starts
+const bodySkin = document.querySelectorAll(".body-skin"),
+    totalBodySkin = bodySkin.length;
+
+for(let i = 0; i < totalBodySkin; i++)
+{
+    bodySkin[i].addEventListener("click", function(){
+        if(this.value === "dark")
+        {
+            document.body.className = "dark";
+        }
+        else
+        {
+            document.body.className = "";
+        }
+    })
+}
+// Body Skin Ends
+
+document.querySelector(".toggle-style-switcher").addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
